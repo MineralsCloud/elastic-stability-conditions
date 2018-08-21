2ac3c9328f518f308786696ab2728105cb203760
Qi Zhang <singularitti@outlook.com>
2018-08-20 22:06:05 -0400

    Separate `ns_conditions` property to 2 parts
    Now they are `ns_conditions_str` property and `ns_conditions` property.

--- escpy/crystal.py
+++ escpy/crystal.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 """
-:mod:`` -- 
+:mod:`` --
 ========================================
 
-.. module 
+.. module
    :platform: Unix, Windows, Mac, Linux
-   :synopsis: 
+   :synopsis:
 .. moduleauthor:: Qi Zhang <qz2280@columbia.edu>
 """
 
 import abc
-from typing import List, Tuple
+from typing import List
 
 import numpy as np
 
@@ -38,13 +38,18 @@ class CrystalSystem:
 
     @property
     @abc.abstractmethod
-    def ns_conditions(self) -> List[Tuple[bool, str]]:
+    def ns_conditions_str(self) -> List[str]:
+        ...
+
+    @property
+    @abc.abstractmethod
+    def ns_conditions(self) -> List[bool]:
         ...
 
     def check_ns_conditions(self, outfile=None) -> bool:
         flag = True
 
-        for criterion, criterion_latex in self.ns_conditions:
+        for criterion, criterion_latex in zip(self.ns_conditions, self.ns_conditions_str):
             if not criterion:  # If `criterion` evaluates to `False`.
                 print("Criterion {0} is not satisfied!".format(criterion_latex), file=outfile)
                 flag = False
@@ -62,14 +67,22 @@ class CrystalSystem:
 
 class CubicSystem(CrystalSystem):
     @property
+    def ns_conditions_str(self):
+        return [
+            "C_{11} > | C_{12} |",
+            "C_{11} + 2 C_{12} > 0",
+            "C_{44} > 0"
+        ]
+
+    @property
     def ns_conditions(self):
         _ = self._elastic_matrix
         c11, c12, c44 = _[0, 0], _[0, 1], _[3, 3]
 
         return [
-            (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-            (c11 + 2 * c12 > 0, "$C_{11} + 2 C_{12} > 0$"),
-            (c44 > 0, "$C_{44} > 0$")
+            c11 > abs(c12),
+            c11 + 2 * c12 > 0,
+            c44 > 0
         ]
 
     def check_symmetry(self):
@@ -78,21 +91,47 @@ class CubicSystem(CrystalSystem):
 
 class HexagonalSystem(CrystalSystem):
     @property
+    def ns_conditions_str(self):
+        return [
+            "C_{11} > | C_{12} |",
+            "2 C_{13}^2 < C_{33} (C_{11} + C_{12})",
+            "C_{44} > 0",
+            "C_{66} > 0"
+
+        ]
+
+    @property
     def ns_conditions(self):
         _ = self._elastic_matrix
         c11, c12, c13, c33, c44 = _[0, 0], _[0, 1], _[0, 2], _[2, 2], _[3, 3]
         c66 = (c11 - c12) / 2
 
         return [
-            (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-            (2 * c13 ** 2 < c33 * (c11 + c12), "$2 C_{13}^2 < C_{33} (C_{11} + C_{12})$"),
-            (c44 > 0, "$C_{44} > 0$"),
-            (c66 > 0, "$C_{66} > 0$")
-
+            c11 > abs(c12),
+            2 * c13 ** 2 < c33 * (c11 + c12),
+            c44 > 0,
+            c66 > 0
         ]
 
 
 class TetragonalSystem(CrystalSystem):
+    @property
+    def ns_conditions_str(self):
+        if self._elastic_matrix[0, 5] == 0:  # Tetragonal (I) class
+            return [
+                "C_{11} > | C_{12} |",
+                "2 C_{13}^2 < C_{33} (C_{11} + C_{12})",
+                "C_{44} > 0",
+                "C_{66} > 0"
+            ]
+
+        return [  # Tetragonal (II) class
+            "C_{11} > | C_{12} |",
+            "2 C_{13}^2 < C_{33} (C_{11} + C_{12})",
+            "C_{44} > 0",
+            "2 C_{16}^2 < C_{66} (C_{11} - C_{12})"
+        ]
+
     @property
     def ns_conditions(self):
         _ = self._elastic_matrix
@@ -100,21 +139,40 @@ class TetragonalSystem(CrystalSystem):
 
         if c16 == 0:  # Tetragonal (I) class
             return [
-                (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-                (2 * c13 ** 2 < c33 * (c11 + c12), "$2 C_{13}^2 < C_{33} (C_{11} + C_{12})$"),
-                (c44 > 0, "$C_{44} > 0$"),
-                (c66 > 0, "$C_{66} > 0$")
+                (c11 > abs(c12)),
+                (2 * c13 ** 2 < c33 * (c11 + c12)),
+                (c44 > 0),
+                (c66 > 0)
             ]
 
         return [  # Tetragonal (II) class
-            (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-            (2 * c13 ** 2 < c33 * (c11 + c12), "$2 C_{13}^2 < C_{33} (C_{11} + C_{12})$"),
-            (c44 > 0, "$C_{44} > 0$"),
-            (2 * c16 ** 2 < c66 * (c11 - c12), "$2 C_{16}^2 < C_{66} (C_{11} - C_{12})$")
+            (c11 > abs(c12)),
+            (2 * c13 ** 2 < c33 * (c11 + c12)),
+            (c44 > 0),
+            (2 * c16 ** 2 < c66 * (c11 - c12))
         ]
 
 
 class RhombohedralSystem(CrystalSystem):
+    @property
+    def ns_conditions_str(self):
+        if self._elastic_matrix[0, 4] == 0:  # Rhombohedral (I) class
+            return [
+                "C_{11} > | C_{12} |",
+                "C_{44} > 0",
+                "C_{13}^2 < 1/2 C_{33} (C_{11} + C_{12})",
+                "C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12})",
+                "C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12}) = C_{44} * C_{66}"
+            ]
+
+        return [  # Rhombohedral (II) class
+            "C_{11} > | C_{12} |",
+            "C_{44} > 0",
+            "C_{13}^2 < 1/2 C_{33} (C_{11} + C_{12})",
+            "C_{14}^2 + C_{15}^2 < 1/2 C_{44} (C_{11} - C_{12})",
+            "C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12}) = C_{44} * C_{66}"
+        ]
+
     @property
     def ns_conditions(self):
         _ = self._elastic_matrix
@@ -122,23 +180,39 @@ class RhombohedralSystem(CrystalSystem):
 
         if c15 == 0:  # Rhombohedral (I) class
             return [
-                (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-                (c44 > 0, "$C_{44} > 0$"),
-                (c13 ** 2 < 0.5 * c33 * (c11 + c12), "$C_{13}^2 < 1/2 C_{33} (C_{11} + C_{12})$"),
-                (c14 ** 2 < 0.5 * c44 * (c11 - c12), "$C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12})$"),
-                (0.5 * c44 * (c11 - c12) == c44 * c66, "$C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12}) = C_{44} * C_{66}$")
+                c11 > abs(c12),
+                c44 > 0,
+                c13 ** 2 < 0.5 * c33 * (c11 + c12),
+                c14 ** 2 < 0.5 * c44 * (c11 - c12),
+                0.5 * c44 * (c11 - c12) == c44 * c66
             ]
 
         return [  # Rhombohedral (II) class
-            (c11 > abs(c12), "$C_{11} > | C_{12} |$"),
-            (c44 > 0, "$C_{44} > 0$"),
-            (c13 ** 2 < 0.5 * c33 * (c11 + c12), "$C_{13}^2 < 1/2 C_{33} (C_{11} + C_{12})$"),
-            (c14 ** 2 + c15 ** 2 < 0.5 * c44 * (c11 - c12), "$C_{14}^2 + C_{15}^2 < 1/2 C_{44} (C_{11} - C_{12})$"),
-            (0.5 * c44 * (c11 - c12) == c44 * c66, "$C_{14}^2 < 1/2 C_{44} (C_{11} - C_{12}) = C_{44} * C_{66}$")
+            c11 > abs(c12),
+            c44 > 0,
+            c13 ** 2 < 0.5 * c33 * (c11 + c12),
+            c14 ** 2 + c15 ** 2 < 0.5 * c44 * (c11 - c12),
+            0.5 * c44 * (c11 - c12) == c44 * c66
         ]
 
 
 class OrthorhombicSystem(CrystalSystem):
+    @property
+    def ns_conditions_str(self):
+        _ = self._elastic_matrix
+        c11, c22, c33, c44, c55, c66 = np.diag(_)
+        c12, c13, c23 = _[0, 1], _[0, 2], _[1, 2]
+
+        return [
+            "C_{11} > 0",
+            "C_{11} C_{22} > C_{12}^2",
+            (c11 * c22 * c33 + 2 * c12 * c13 * c23 > c11 * c23 ** 2 + c22 * c13 ** 2 + c33 * c12 ** 2,
+             "C_{11} C_{22} C_{33} + 2 C_{12} C_{13} C_{23} > C_{11} C_{23}^2 + C_{22} C_{13}^2 + C_{33} C_{12}^2"),
+            "C_{44} > 0",
+            "C_{55} > 0",
+            "C_{66} > 0"
+        ]
+
     @property
     def ns_conditions(self):
         _ = self._elastic_matrix
@@ -146,17 +220,28 @@ class OrthorhombicSystem(CrystalSystem):
         c12, c13, c23 = _[0, 1], _[0, 2], _[1, 2]
 
         return [
-            (c11 > 0, "$C_{11} > 0$"),
-            (c11 * c22 > c12 ** 2, "$C_{11} C_{22} > C_{12}^2$"),
-            (c11 * c22 * c33 + 2 * c12 * c13 * c23 > c11 * c23 ** 2 + c22 * c13 ** 2 + c33 * c12 ** 2,
-             "$C_{11} C_{22} C_{33} + 2 C_{12} C_{13} C_{23} > C_{11} C_{23}^2 + C_{22} C_{13}^2 + C_{33} C_{12}^2$"),
-            (c44 > 0, "$C_{44} > 0$"),
-            (c55 > 0, "$C_{55} > 0$"),
-            (c66 > 0, "$C_{66} > 0$")
+            c11 > 0,
+            c11 * c22 > c12 ** 2,
+            c11 * c22 * c33 + 2 * c12 * c13 * c23 > c11 * c23 ** 2 + c22 * c13 ** 2 + c33 * c12 ** 2,
+            c44 > 0,
+            c55 > 0,
+            c66 > 0
         ]
 
 
 class MonoclinicSystem(CrystalSystem):
+    @property
+    def ns_conditions_str(self):
+        return [
+            "C_{ii} > 0",
+            "C_{11} + C_{22} + C_{33} + 2 * (C_{12} + C_{13} + C_{23})",
+            "C_{33} * C_{55} - C_{35}^2 > 0",
+            "C_{44} * C_{66} - C_{46}^2 > 0",
+            "C_{22} + C_{33} - 2 * C_{23}  > 0",
+            "C_{22} * (C_{33} * C_{55} - C_{35}^2) + 2 * C_{23} * C_{25} * C_{35} - c23^2 * c55 - c25^2 * c33> 0",
+            "The last criterion"
+        ]
+
     @property
     def ns_conditions(self):
         _ = self._elastic_matrix
@@ -165,23 +250,24 @@ class MonoclinicSystem(CrystalSystem):
         g = c11 * c22 * c33 - c11 * c23 * c23 - c22 * c13 * c13 - c33 * c12 * c12 + 2 * c12 * c13 * c23
 
         return [
-            (all(_ > 0 for _ in (c11, c22, c33, c44, c55, c66)), "$C_{ii} > 0$"),
-            (c11 + c22 + c33 + 2 * (c12 + c13 + c23) > 0,
-             "$C_{11} + C_{22} + C_{33} + 2 * (C_{12} + C_{13} + C_{23})$"),
-            (c33 * c55 - c35 ** 2 > 0, "$C_{33} * C_{55} - C_{35}^2 > 0$"),
-            (c44 * c66 - c46 ** 2 > 0, "$C_{44} * C_{66} - C_{46}^2 > 0$"),
-            (c22 + c33 - 2 * c23 > 0, "$C_{22} + C_{33} - 2 * C_{23}  > 0$"),
-            (c22 * (c33 * c55 - c35 ** 2) + 2 * c23 * c25 * c35 - c23 ** 2 * c55 - c25 ** 2 * c33 > 0,
-             "$C_{22} * (C_{33} * C_{55} - C_{35}^2) + 2 * C_{23} * C_{25} * C_{35} - c23^2 * c55 - c25^2 * c33> 0$"),
-            (2 * (c15 * c25 * (c33 * c12 - c13 * c23) + c15 * c35 * (c22 * c13 - c12 * c23) + c25 * c35 * (
+            all(_ > 0 for _ in (c11, c22, c33, c44, c55, c66)),
+            c11 + c22 + c33 + 2 * (c12 + c13 + c23) > 0,
+            c33 * c55 - c35 ** 2 > 0,
+            c44 * c66 - c46 ** 2 > 0,
+            c22 + c33 - 2 * c23 > 0,
+            c22 * (c33 * c55 - c35 ** 2) + 2 * c23 * c25 * c35 - c23 ** 2 * c55 - c25 ** 2 * c33 > 0,
+            2 * (c15 * c25 * (c33 * c12 - c13 * c23) + c15 * c35 * (c22 * c13 - c12 * c23) + c25 * c35 * (
                     c11 * c23 - c12 * c13)) - (
-                     c15 * c15 * (c22 * c33 - c23 ^ 2) + c25 * c25 * (c11 * c33 - c13 ^ 2) + c35 * c35 * (
-                     c11 * c22 - c12 ^ 2)) + c55 * g > 0,
-             "The last criterion")
+                    c15 * c15 * (c22 * c33 - c23 ^ 2) + c25 * c25 * (c11 * c33 - c13 ^ 2) + c35 * c35 * (
+                    c11 * c22 - c12 ^ 2)) + c55 * g > 0,
         ]
 
 
 class TriclinicSystem(CrystalSystem):
     @property
-    def ns_conditions(self):
+    def ns_conditions_str(self):
         return []
+
+    @property
+    def ns_conditions(self):
+        pass
