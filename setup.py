5c7b1129392dd0278893b00ead0ae88017fdcf1a
Qi Zhang <singularitti@outlook.com>
2019-01-02 00:15:57 -0500

    Deprecate Python 2 support
    not possible

--- setup.py
+++ setup.py
@@ -22,12 +22,11 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
+    python_requires='>=3.5, <4',
     install_requires=[
         'numpy'
     ],
