e0d6002ee1aa1e6d46ea35f821f330e55ca3d243
Qi Zhang <singularitti@outlook.com>
2019-01-02 00:13:38 -0500

    Update setup.py
    for Python versions

--- setup.py
+++ setup.py
@@ -23,12 +23,11 @@ setup(
         'Topic :: Scientific/Engineering :: Physics',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
-    python_requires='>=2.7,>=3.4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, <4',
     install_requires=[
         'numpy'
     ],
